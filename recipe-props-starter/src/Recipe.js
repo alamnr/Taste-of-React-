9d7a7f2bb4f08c05326f6cecdf440383da769c82
alamnr <alamnr@gmail.com>
2019-12-04 15:45:31 +0600

    react default props and prop types

--- recipe-props-starter/src/Recipe.js
+++ recipe-props-starter/src/Recipe.js
@@ -1,36 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Recipe.css'
 
 
 class Recipe extends Component {
     static defaultProps = {
-        ingridients : []
+        ingridients: []
     }
     static propTypes = {
-         ingridients: PropTypes.arrayOf(PropTypes.string).isRequired   
+        title: PropTypes.string.isRequired,
+        ingridients: PropTypes.arrayOf(PropTypes.string).isRequired,
+        instruction: PropTypes.string.isRequired,
+        img: PropTypes.string.isRequired
     }
-    render(){
-        const {title, instruction, img} = this.props;
-        const ingridients = this.props.ingridients.map((ing,index)=> (<li key={index}>{ing}</li>))
-    
-        return(
-            <div className="recipe-card">
-                <div className="recipe-card-img">
-                    <img src={img} alt={title} width='300px' height='300px'/>
-                </div>
-                <div className="recipe-card-content">
-                <h3 className="recipe-title">Recipe {title} </h3>
-                <h4>Ingridients:</h4>
-                <ul>
-                    {ingridients}
-                </ul>
-                <h4>Instructions:</h4>
-                <p>{instruction}</p>    
-                </div>
-                
-            </div>
+    render() {
+        const { title, instruction, img } = this.props;
+        const ingridients = this.props.ingridients.map((ing, index) => (<li key={index}>{ing}</li>))
+
+        return (
             
+                <div className="recipe-card">
+                    <div className="recipe-card-img">
+                        <img src={img} alt={title} width='300px' height='300px' />
+                    </div>
+                    <div className="recipe-card-content">
+                        <h3 className="recipe-title">{title} </h3>
+                        <h4>Ingridients:</h4>
+                        <ul>
+                            {ingridients}
+                        </ul>
+                        <h4>Instructions:</h4>
+                        <p>{instruction}</p>
+                    </div>
+
+                </div>
+            
+
+
         );
 
     }
--- recipe-props-starter/src/RecipeApp.js
+++ recipe-props-starter/src/RecipeApp.js
@@ -2,21 +2,48 @@ import React, {Component} from 'react';
 //import logo from './logo.svg';
 import './RecipeApp.css';
 import Recipe from './Recipe';
+import Navbar from './Navbar';
+import PropTypes from 'prop-types';
 
 class RecipeApp extends Component {
   static defaultProps = {
     recipes: [{
       title: 'Burger',
       ingridients: ['flower', 'water', 'meat'],
-      //ingridients: [1, 2, 3],
       instruction: "Mix ingridients",
       img: "https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg"
-    }]
+    },
+    {
+      title: 'Role',
+      ingridients: ['flower', 'water', 'meat', 'vege'],
+      instruction: "Mix ingridients",
+      img: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg"
+    },
+    {
+      title: 'Shrimp',
+      ingridients: ['flower', 'water', 'shrimp'],
+      instruction: "Mix ingridients",
+      img: "https://images.pexels.com/photos/255501/pexels-photo-255501.jpeg"
+    },
+    {
+      title: 'Pizza',
+      ingridients: ['flower', 'water', 'meat','baking'],
+      instruction: "Mix ingridients",
+      img: "https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg"
+    }  
+  ]
   }
+
+  static propTypes = {
+    recipes : PropTypes.arrayOf(PropTypes.Object).isRequired
+  }
+
   render() {
 
     return (
       <div className="App">
+        <Navbar />
+        <div className="recipe-list">
         {this.props.recipes.map((r, index) => (
           // <Recipe key={index}
           //   title={r.title}
@@ -27,8 +54,8 @@ class RecipeApp extends Component {
           //  Using REST operator 
           <Recipe key={index} {...r} />
         ))} 
-        
-        
+        </div>
+               
 
       </div>
     );
